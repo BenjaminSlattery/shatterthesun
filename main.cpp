ea66dcf5a62cdff49edbd8f479804c65ce7bd581
BenjaminSlattery <50343293+BenjaminSlattery@users.noreply.github.com>
2026-04-14 18:20:12 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -29,4 +29,36 @@ int main() {
     return 0;
 }
 
+#include "imgui.h"
+#include "imgui_impl_glfw.h"
+#include "imgui_impl_opengl3.h"
+#include <GLFW/glfw3.h>
+
+int main() {
+    glfwInit();
+    GLFWwindow* win = glfwCreateWindow(800, 600, "ImGui Demo", nullptr, nullptr);
+    glfwMakeContextCurrent(win);
+
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    ImGui_ImplGlfw_InitForOpenGL(win, true);
+    ImGui_ImplOpenGL3_Init("#version 130");
+
+    while (!glfwWindowShouldClose(win)) {
+        glfwPollEvents();
+        ImGui_ImplOpenGL3_NewFrame();
+        ImGui_ImplGlfw_NewFrame();
+        ImGui::NewFrame();
+
+        ImGui::Begin("Hello");
+        ImGui::Text("It works!");
+        ImGui::End();
+
+        ImGui::Render();
+        glClear(GL_COLOR_BUFFER_BIT);
+        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+        glfwSwapBuffers(win);
+    }
+    return 0;
+}
 
