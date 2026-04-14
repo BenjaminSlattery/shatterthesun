02dac934e019ab4757a74d710279e43a1826a726
BenjaminSlattery <50343293+BenjaminSlattery@users.noreply.github.com>
2026-04-14 18:20:49 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,29 +6,6 @@
 
 using namespace std;
 
-int main() {
-    cout << "Hello, C++ Cloud IDE!" << endl;
-
-    vector<string> features = {
-        "Blazing fast cloud compilation",
-        "Monaco Editor integration",
-        "Live OpenGL virtual display",
-        "Audio output (instant, no delay)"
-    };
-
-    Screen *screen = new Screen();
-    //screen->Update();
-    //screen->Print();
-
-    int name;
-
-    cin >> name;
-
-    cout << name;
-
-    return 0;
-}
-
 #include "imgui.h"
 #include "imgui_impl_glfw.h"
 #include "imgui_impl_opengl3.h"
