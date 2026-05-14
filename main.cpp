5f5c8ad520f00ab1e375102d425c7420a6b2a193
BenjaminSlattery <50343293+BenjaminSlattery@users.noreply.github.com>
2026-05-14 11:25:26 -0400

    main.cpp: render full 80x50 screen via screen->Draw() in a fullscreen ImGui viewport (fixes -Werror=format-security)

--- main.cpp
+++ main.cpp
@@ -1,44 +1,55 @@
 #include "masterheader.h"
-#include <iostream>
-#include <string>
-#include <vector>
-#include "screen.h"
+  #include <iostream>
+  #include <string>
+  #include <vector>
+  #include "screen.h"
 
-#include "imgui.h"
-#include "imgui_impl_glfw.h"
-#include "imgui_impl_opengl3.h"
-#include <GLFW/glfw3.h>
+  #include "imgui.h"
+  #include "imgui_impl_glfw.h"
+  #include "imgui_impl_opengl3.h"
+  #include <GLFW/glfw3.h>
 
-using namespace std;
+  using namespace std;
 
-int main() {
-    
-    glfwInit();
-    GLFWwindow* win = glfwCreateWindow(800, 600, "ImGui Demo", nullptr, nullptr);
-    glfwMakeContextCurrent(win);
+  int main() {
 
-    Screen *screen = new Screen();
+      glfwInit();
+      GLFWwindow* win = glfwCreateWindow(800, 600, "ImGui Demo", nullptr, nullptr);
+      glfwMakeContextCurrent(win);
 
-    IMGUI_CHECKVERSION();
-    ImGui::CreateContext();
-    ImGui_ImplGlfw_InitForOpenGL(win, true);
-    ImGui_ImplOpenGL3_Init("#version 130");
+      Screen *screen = new Screen();
 
-    while (!glfwWindowShouldClose(win)) {
-        glfwPollEvents();
-        ImGui_ImplOpenGL3_NewFrame();
-        ImGui_ImplGlfw_NewFrame();
-        ImGui::NewFrame();
+      IMGUI_CHECKVERSION();
+      ImGui::CreateContext();
+      ImGui_ImplGlfw_InitForOpenGL(win, true);
+      ImGui_ImplOpenGL3_Init("#version 130");
 
-        ImGui::Begin("Hello");
-        ImGui::Text(screen->screen[0]);
-        ImGui::End();
+      while (!glfwWindowShouldClose(win)) {
+          glfwPollEvents();
+          ImGui_ImplOpenGL3_NewFrame();
+          ImGui_ImplGlfw_NewFrame();
+          ImGui::NewFrame();
 
-        ImGui::Render();
-        glClear(GL_COLOR_BUFFER_BIT);
-        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
-        glfwSwapBuffers(win);
-    }
-    return 0;
-}
+          // Fullscreen ImGui window so screen->Draw()'s auto-scaling fills the
+          // OS window. NoTitleBar/NoResize/NoMove make it feel like a canvas
+          // rather than a draggable subwindow.
+          const ImGuiViewport* vp = ImGui::GetMainViewport();
+          ImGui::SetNextWindowPos(vp->Pos);
+          ImGui::SetNextWindowSize(vp->Size);
+          ImGui::Begin("ScreenGrid", nullptr,
+              ImGuiWindowFlags_NoTitleBar |
+              ImGuiWindowFlags_NoResize |
+              ImGuiWindowFlags_NoMove |
+              ImGuiWindowFlags_NoCollapse |
+              ImGuiWindowFlags_NoBringToFrontOnFocus);
+          screen->Draw();
+          ImGui::End();
 
+          ImGui::Render();
+          glClear(GL_COLOR_BUFFER_BIT);
+          ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+          glfwSwapBuffers(win);
+      }
+      return 0;
+  }
+  
