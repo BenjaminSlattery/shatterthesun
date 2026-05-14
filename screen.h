8fddc2b04a6ad82f1cb62d1938f060ec64c156e9
BenjaminSlattery <50343293+BenjaminSlattery@users.noreply.github.com>
2026-05-14 13:46:52 -0400

    Update screen.h

--- screen.h
+++ screen.h
@@ -17,8 +17,9 @@
 
         Screen() {
             for (int x = 0; x < WIDTH; x++)
-                for (int y = 0; y < HEIGHT; y++) {}
+                for (int y = 0; y < HEIGHT; y++) {
                     screen[x][y] = " ";
+                
             Update();
         }
 
